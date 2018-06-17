ae1b9bbd8953d4930b41a808dae5369bdc0d389b
rxluz <rx@appock.co>
2018-06-17 10:52:30 +0100

    post data

--- server/controllers/threads.controller.js
+++ server/controllers/threads.controller.js
@@ -1,3 +1,5 @@
+
+
 function get(req, res) {
   const content = [
     {
@@ -34,7 +36,7 @@ function get(req, res) {
     },
     {
       id: 19,
-      content: 'Where can I find the chepeast beer in this area?',
+      content: 'Anyone want meet up for random beers?',
       votes: {
         total: 23,
         is: {
@@ -178,12 +180,115 @@ function setDownVoteComment(req, res) {
 }
 
 function post(req, res) {
-  return res.send({
+  const content = [
+    {
+      id: 21,
+      content: 'Has anyone seen my laptop in Murphy\'s Pub? I left it there last night :(',
+      votes: {
+        total: 32,
+        is: {
+          up: false,
+          down: true
+        }
+      },
+      date: {
+        timestamp: 1529222592,
+        friendly: '13 minutes ago'
+      },
+      author: 'dan'
+    },
+    {
+      id: 20,
+      content: 'Are there any dangerous streets in this area?',
+      votes: {
+        total: 27,
+        is: {
+          up: false,
+          down: true
+        }
+      },
+      date: {
+        timestamp: 1529222492,
+        friendly: '2 hours ago'
+      },
+      author: 'lola'
+    },
+    {
+      id: 19,
+      content: 'Anyone want meet up for random beers?',
+      votes: {
+        total: 23,
+        is: {
+          up: false,
+          down: false
+        }
+      },
+      date: {
+        timestamp: 1529222392,
+        friendly: '9 days ago'
+      },
+      author: 'rxluz'
+    },
+    {
+      id: 18,
+      content: 'Does anyone know a good route to Temple bar for me, I use a wheelchair',
+      votes: {
+        total: 15,
+        is: {
+          up: false,
+          down: false
+        }
+      },
+      date: {
+        timestamp: 1529222392,
+        friendly: '3 hours ago'
+      },
+      author: 'dan'
+    },
+    {
+      id: 17,
+      content: 'Hi guys, I really need an iPhone charger, anyone around have one?',
+      votes: {
+        total: 9,
+        is: {
+          up: false,
+          down: false
+        }
+      },
+      date: {
+        timestamp: 1529222392,
+        friendly: '5 minutes ago'
+      },
+      author: 'lola'
+    },
+    {
+      id: 17,
+      content: 'Steer clear off Canden street? A bus has just hit a car so it is closed!',
+      votes: {
+        total: 3,
+        is: {
+          up: false,
+          down: false
+        }
+      },
+      date: {
+        timestamp: 1529222392,
+        friendly: '18 minutes ago'
+      },
+      author: 'lola'
+    },
+  ];
+  const contentInt = {
     id: 90, 
     content: req.body.content,
     timer: req.body.timer,
     price: req.body.price
-  });
+  };
+
+  content.unshift(contentInt);
+
+
+  return res.json({ total: content.length, data: content });
 }
 
 
